13e90b19c207071936c626d8135dacaa2e33a599
Ryan Carroll <software@ryancarroll.io>
2022-11-20 19:04:55 -0500

    initial commit

--- Roshamb0.py
+++ Roshamb0.py
@@ -9,6 +9,7 @@ paper = 2
 scissors = 3
 cpu_choice = 0
 user_choice = 0
+valid_inputs = False
 
 def main():
     cpu_choice()
@@ -19,9 +20,19 @@ def cpu_choice():
     print(cpu_choice)
 
 def user_choice():
-    userInput = input(print("Select Rock, Paper or Scissors: "))
-    userInput = userInput.lower()
-    rock_re.search
+    while True:
+        try:
+            userInput = input(print("Select Rock, Paper or Scissors: "))
+            userInput = userInput.lower()
+            rock_re = re.compile("rock", flags=re.I)
+            paper_re = re.compile("paper", flags=re.I)
+            scissors_re = re.compile("scissors", flags=re.I)
+            valid_inputs = [rock_re, paper_re, scissors_re]
+            return
+        else:
+            print('Bad choice')
+            break
 
-
+def is_input_valid(user_choice):
+    return any([x.search(user_choice) is not none for x in valid_inputs])
 main()
--- stadiumSeating.py
+++ stadiumSeating.py
@@ -1,1 +1,22 @@
+# Ryan Carroll
+# CIS_115_NLE01
+# stadiumSeating.py
 
+class_a = 0
+class_b = 0
+class_c = 0
+total_sales = 0
+def main():
+    get_tickets_sold()
+
+def get_tickets_sold():
+    class_a = input("How many Class A tickets were sold?")
+    class_b = input("How many Class B tickets were sold?")
+    class_c = input("How many Class C tickets were sold?")
+    return
+def calc_total_sales():
+    total_a = class_a * 20
+    total_b = class_b * 15
+    total_c = class_c * 10
+    total_sales = total_a + total_b + total_c
+    return total_sales
