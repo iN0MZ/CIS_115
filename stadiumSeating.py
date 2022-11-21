6128d3ce57018aa283aa1a3b6c32e35cb7979b1c
Ryan Carroll <software@ryancarroll.io>
2022-11-20 22:13:14 -0500

    end

--- stadiumSeating.py
+++ stadiumSeating.py
@@ -1,22 +1,32 @@
 # Ryan Carroll
 # CIS_115_NLE01
 # stadiumSeating.py
+# Converts ticket types into correct dollar amount then adds the total
 
-class_a = 0
-class_b = 0
-class_c = 0
-total_sales = 0
+a_value = 20    #
+b_value = 15    # Sets the ticket prices as constants
+c_value = 10    #
+
 def main():
-    get_tickets_sold()
+    num1, num2, num3 = get_tickets_sold()  # Calls get tickets sold and sets output to num 1,2,3
+    total_sum = calc_total_sales(num1,num2,num3) # Calls calc_total_sales passing num 1,2,3 - Sets to total_sum variable
+    output(total_sum) # calls total sum function
 
-def get_tickets_sold():
-    class_a = input("How many Class A tickets were sold?")
-    class_b = input("How many Class B tickets were sold?")
-    class_c = input("How many Class C tickets were sold?")
-    return
-def calc_total_sales():
-    total_a = class_a * 20
-    total_b = class_b * 15
-    total_c = class_c * 10
-    total_sales = total_a + total_b + total_c
-    return total_sales
+def get_tickets_sold(): # Asks the user for input then sets the input to variables num 1,2,3
+    num1 = int(input("How many Class A tickets were sold? "))
+    num2 = int(input("How many Class B tickets were sold? "))
+    num3 = int(input("How many Class C tickets were sold? "))
+    return int(num1), int(num2), int(num3) # Returns the integer of each variable
+
+def calc_total_sales(num1, num2, num3): # Does math and outputs total_sum
+    total_a = num1 * a_value
+    total_b = num2 * b_value
+    total_c = num3 * c_value
+    total_sum = total_a + total_b + total_c
+    return total_sum
+
+def output(total_sum): # Prints the total sum variable
+    print(f"${total_sum}")
+
+
+main()
