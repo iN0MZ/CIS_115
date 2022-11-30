cafd39d4fff4ceaf5a9e1c3d8863073fcd134e64
NOMZ <113869368+iN0MZ@users.noreply.github.com>
2022-11-30 18:39:40 -0500

    initial commit

---
+++ Notes etc/11_30_2022.py
@@ -0,0 +1,18 @@
+def main():
+    print('enter two variables')
+    result = None
+    try:
+
+        integer1 = int(input())
+        integer2 = int(input())
+        result = integer1/integer2
+    except ValueError as err:
+        print(err)
+        print("You entered the wrong data type")
+    except ZeroDivisionError:
+        print('Cannot divide by 0, please try again.')
+    except:
+        print("An unknown error has occurred.")
+    print(result)
+
+main()
