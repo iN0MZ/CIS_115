c278dc07269e15687ffe119b8537b016924247cc
Ryan Carroll <software@ryancarroll.io>
2022-12-11 18:18:48 -0500

    Logic works

--- Roshamb0.py
+++ Roshamb0.py
@@ -2,37 +2,74 @@
 # Ryan Carroll
 
 import random
-import re
-
-rock = 1
-paper = 2
-scissors = 3
-cpu_choice = 0
-user_choice = 0
-valid_inputs = False
-
+player_roll = None
+cpu_roll = None
 def main():
-    cpu_choice()
-    user_choice()
+    cpu_roll = cpu_choice()
+    userInput = user_choice()
+    player_roll = numAssign(userInput)
+
+    comparison(player_roll, cpu_roll)
+
 
 def cpu_choice():
-    cpu_choice = random.choice([1,2,3])
-    print(cpu_choice)
-
+    cpu_select = random.choice([1, 2, 3])
+    if cpu_select == 1:
+        print("The computer picked Rock!")
+    elif cpu_select == 2:
+        print("The computer picked Paper!")
+    elif cpu_select == 3:
+        print("The computer picked Scissors!")
+    return cpu_select
 def user_choice():
-    while True:
-        try:
-            userInput = input(print("Select Rock, Paper or Scissors: "))
-            userInput = userInput.lower()
-            rock_re = re.compile("rock", flags=re.I)
-            paper_re = re.compile("paper", flags=re.I)
-            scissors_re = re.compile("scissors", flags=re.I)
-            valid_inputs = [rock_re, paper_re, scissors_re]
-            return
-        else:
-            print('Bad choice')
-            break
+    userInput = input(str("Select Rock, Paper or Scissors: "))
+    acceptedInput = ['rock', 'paper', 'scissors']
+    if userInput.lower() not in acceptedInput:
+        print("Invalid choice. Please try again.")
+    print("User_Choice function: Much Success")
+    return userInput.lower()
 
-def is_input_valid(user_choice):
-    return any([x.search(user_choice) is not none for x in valid_inputs])
+
+
+# def is_input_valid(user_choice):
+#     return any([x.search(user_choice) is not none for x in valid_inputs])
+
+
+def numAssign(userInput):
+    if userInput == "rock":
+        player_choice = 1
+    elif userInput == "paper":
+        player_choice = 2
+    elif userInput == "scissors":
+        player_choice = 3
+    return player_choice
+
+
+
+def comparison(player_roll, cpu_roll):
+    if player_roll == 1 and cpu_roll == 2:
+        return print("Paper covers Rock\n You Lose!")
+
+    elif player_roll == 1 and cpu_roll == 3:
+        return print("Rock smashes Scissors\nYou Win!")
+
+    elif player_roll == 2 and cpu_roll == 1:
+        return print("Paper covers Rock\nYou Win!")
+
+    elif player_roll == 2 and cpu_roll == 3:
+        return print("Scissors cuts Paper\nYou Lose!")
+
+    elif player_roll == 3 and cpu_roll == 1:
+        return print("Rock smashes Scissors\nYou Lose!")
+
+    elif player_roll == 3 and cpu_roll == 2:
+        return print("Scissors cuts Paper\nYou Win!")
+
+    elif player_roll == cpu_roll:
+        return print("Tie")
+
+
+
+
 main()
+
