ccab938bdeabff7c7196ba59b68356349f3dc21f
Ryan Carroll <software@ryancarroll.io>
2022-12-12 17:13:57 -0500

    Game works as intended. Comments added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,7 +10,14 @@ cpu_roll = None
 
 ########################################
 # Main: while loop prints the title img, copywrite info, as well as wins and losses
-#
+# defines cpu_roll, user_str_input and player_roll
+# cpu_roll = 1, 2 or 3 assigned by cpu_choice function
+# user_str_input = 'rock' 'paper' or 'scissors'
+# player_roll = 1, 2 or 3 now after passing through numAssign()
+# while loop prompts the user to play and gets both cpu and user rolls
+# calls comparison() for game logic
+# Displays wins and losses again and prompts the user to play again
+# clear() to wipe the console and start over
 
 def main():
     wins = 0
@@ -31,15 +38,20 @@ def main():
         clear()
 
 
+#####################################################
+# cpu_choice(): gets a random number for the computer
+# 1, 2 or 3 is the output
+#
+
 def cpu_choice():
     cpu_select = random.choice([1, 2, 3])
     return cpu_select
 
 
-##################################################################
-# user_choice: Takes user input as a string
+################################################################
+# user_choice(): Takes user input as a string
 # defines what input is acceptable
-# checks to
+# checks to see if the input matches and if not restarts the loop
 
 def user_choice():
     userInput = None
@@ -59,8 +71,8 @@ def user_choice():
 # "this is not the efficient way"
 # You could remove this function by using a dictionary instead
 # This function takes the string input and changes it to an integer item key
-#
 # importing user_str_input and checking it against each possible output
+# Export player_choice
 def numAssign(user_str_input):
     if user_str_input == "rock":
         player_choice = 1
@@ -70,6 +82,14 @@ def numAssign(user_str_input):
         player_choice = 3
     return player_choice
 
+
+##################################################################
+# comparison(): Takes the following arguments:
+# player_roll, cpu_roll and user_str_input
+# Calculates the different game outcomes and will assign a score to
+# result +/- 1
+# Draws art through print statements
+# Returns result
 
 def comparison(player_roll, cpu_roll, user_str_input):
     result = 0
@@ -103,9 +123,19 @@ def comparison(player_roll, cpu_roll, user_str_input):
     return result
 
 
+################################################
+# clear(): This function wipes the console with new lines
+#
 def clear():
     print("\n" * 47)
 
+
+#############################################################
+# The if __name__ == "__main__": statement is used to check whether the code is being
+# run directly from the command line or being called from another module.
+# If the code is being run directly from the command line, then the if statement
+# evaluates to True and the main() function is called.
+#
 
 if __name__ == "__main__":
     main()
