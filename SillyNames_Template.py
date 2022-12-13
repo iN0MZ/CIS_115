75d044d7a3f85fe1d8c712765355f2ae62dec2be
Ryan Carroll <software@ryancarroll.io>
2022-12-13 10:08:08 -0500

    initial commit

---
+++ SillyNames_Template.py
@@ -0,0 +1,115 @@
+##Funny Name Generator Project
+##Using Tuples 
+
+import random
+
+def main():
+    ##the following print statements will be provided to you
+    print("Welcome to the Silly Name generator.")
+    print("Generate a list of names for your sidekick/friend/pet.")
+    print("The 10 names will be exported to a .txt file!")
+    # userInput()
+    tupleCreate()
+
+    ##Create Tuple with given first names.
+    # first_name_file = (open('first_names_silly_name_generator.txt', 'r'))
+    # print({first})
+    # ##create tuple with given last names.
+    # last = open('last_names_silly_name_generator.txt', 'r')
+
+    ##create a function called userInput() to receive the name of the file.
+
+def userInput():
+    firstnames = input()
+    file_contents = infile.read()
+    print(file_contents)
+
+
+    input("\nHit Enter to Generate.\n")
+
+
+    ##create a function called "createFile"
+    ##that only takes user's file name and returns a file object.
+
+
+    ##place your generator file here. It is a void function that 
+    ##takes 3 arguments.
+
+
+
+    print("\n\nFile has been created.\n")
+    input("Hit enter to Quit")
+
+def userInput():
+    ##This function takes no parameters
+
+    ##this function takes user input for a file name.
+
+    ##Input validation if '.txt' is does not exist add it
+
+    ##return the file name string
+    user = input("\nPlease enter a file name:\n")
+
+def tupleCreate():
+    mylist = None
+    with open('first_names_silly_name_generator.txt', 'r') as f:
+        mylist = [tuple(map(float, i.split(','))) for i in f]
+
+    print(mylist)
+    # ('word1','word2','word3','word4','word5')
+
+
+#     if '.txt' not in user:
+#
+#         ##connact .txt to name
+#
+#
+# def createFile():
+#     ##this function takes one parameter called "file name"
+#
+#     ##open a new file in mode 'w' with the given file name.
+#
+#     ##return the created file. Do NOT close file here.
+#
+#
+# def generateNames(firstName, lastName, file):
+#
+#     ##Randomly generate names with a for loop 10 times
+#
+#     ##you must generate by first getting a randomly generated index THEN
+#     ##assign it to either first or last
+#
+#     ##append/write each first + last name to the file
+#     ##Each name MUST be on a new line!!
+#
+#     ##you will want to remove the names from the new coped list to ensure
+#     ##no names are choosen twice!
+#
+#     ##print out the names in console
+#
+#
+#     ##close file outside of the for loop!
+#
+#     firstName = list(firstName)
+#     lastName = list(lastName)
+#
+#     print("\t\tYour Generated Names:")
+#     ##Your for loop
+#       ##find a random index in your list
+#       ##
+#
+#       ##find the item by using the index
+#       ##
+#
+#       ##remove the items from list
+#       ##
+#
+#       ##make a variable that has first + last names
+#
+#       ##print
+#
+#       ##write to file
+#
+#
+
+main()
